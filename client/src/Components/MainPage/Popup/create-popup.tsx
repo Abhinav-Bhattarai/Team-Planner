0948bc27e6136b3062bc8b1e6850b2ca30bf6cb8
Abhinav Bhattarai <messibhattarai3@gmail.com>
2021-04-28 21:20:25 +0545

    FE: Implementing Cropper

--- client/src/Components/MainPage/Popup/create-popup.tsx
+++ client/src/Components/MainPage/Popup/create-popup.tsx
@@ -1,4 +1,7 @@
-import React, { useRef, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
+import Cropper from "react-easy-crop";
+import { Point, Area } from "react-easy-crop/types";
+import getCroppedImage from './getCroppedImage';
 import {
   PopupButton,
   PopupContainer,
@@ -8,10 +11,52 @@ import {
   PopupTextInput,
 } from "./Reusables/reusables";
 
-const CreatePopup: React.FC<{ Submit: (event: any, name: string, profile: string) => void }> = (props) => {
+const CropperContainer: React.FC<{}> = (props) => {
+  const { children } = props;
+  return (
+    <main
+      style={{
+        position: "relative",
+        height: "350px",
+        backgroundColor: "#2a2c30",
+      }}
+    >
+      {children}
+    </main>
+  );
+};
+
+const CropperButton: React.FC<{Submit: () => void}> = (props) => {
+  const { Submit } = props;
+  return (
+    <button 
+      onClick={Submit}
+      style={{
+        backgroundColor: '#44b581',
+        color: '#fff',
+        fontWeight: 'bold',
+        border: 'none',
+        padding: '12px 0',
+        margin: '10px 3%',
+        borderRadius: '10px'
+      }}
+    >
+      Finalize
+    </button>
+  )
+}
+
+const CreatePopup: React.FC<{
+  Submit: (event: any, name: string, profile: string) => void;
+}> = (props) => {
   const { Submit } = props;
   const [name, SetName] = useState<string>("");
   const [profile, SetProfile] = useState<string>("");
+  const [zoom, setZoom] = useState<number>(1);
+  const [crop, setCrop] = useState<{ x: number; y: number }>({ x: 10, y: 10 });
+  const [cropper, SetCropper] = useState<boolean>(false);
+  const [croppedAreaPixel, setCroppedAreaPixel] = useState<Area | null>(null);
+  const [croppedImage, setCroppedImage] = useState<string | null>(null);
   const InputRef = useRef(null);
 
   const HandleNameChange = (event: any): void => {
@@ -25,6 +70,7 @@ const CreatePopup: React.FC<{ Submit: (event: any, name: string, profile: string
     reader.onloadend = () => {
       // @ts-ignore
       SetProfile(reader.result);
+      SetCropper(true);
     };
     reader.readAsDataURL(img_file);
   };
@@ -36,10 +82,18 @@ const CreatePopup: React.FC<{ Submit: (event: any, name: string, profile: string
     }
   };
 
-  return (
-    <React.Fragment>
-      <PopupContainer>
-        <PopupHeader title="Create Team" />
+  const onCropComplete = useCallback((Area: Area, AreaPixels: Area) => {}, []);
+
+  const FinalizeCropping = async() => {
+    // @ts-ignore
+    const cropped_image: string = getCroppedImage(profile, croppedAreaPixel);
+    setCroppedImage(cropped_image);
+    SetCropper(false);
+  };
+
+  let PopupContents = () => {
+    return (
+      <React.Fragment>
         <PopupForm Submit={(e: any) => Submit(e, name, profile)}>
           <PopupTextInput
             name="group-name"
@@ -53,9 +107,47 @@ const CreatePopup: React.FC<{ Submit: (event: any, name: string, profile: string
           <PopupSelector title="Select Profile" Submit={SelectProfileHandler} />
           <PopupButton title="Create" />
         </PopupForm>
+      </React.Fragment>
+    );
+  };
+
+  const CropChangeHandler = (location: Point) => {
+    if (location.x !== crop.x || location.y !== crop.y) {
+      setCrop(location);
+    }
+  }
+
+  console.log(crop);
+
+  if (cropper && profile.length > 10) {
+    PopupContents = () => {
+      return (
+        <React.Fragment>
+          <CropperContainer>
+            <Cropper
+              image={profile}
+              crop={crop}
+              zoom={zoom}
+              aspect={37.7777 / 45}
+              onCropChange={(location: Point) => console.log(location)}
+              onCropComplete={onCropComplete}
+              onZoomChange={(zoom_count: number) => setZoom(zoom_count)}
+            />
+          </CropperContainer>
+          <CropperButton Submit={FinalizeCropping}/>
+        </React.Fragment>
+      );
+    };
+  }
+
+  return (
+    <React.Fragment>
+      <PopupContainer>
+        <PopupHeader title="Create Team" />
+        <PopupContents />
       </PopupContainer>
     </React.Fragment>
   );
 };
 
-export default CreatePopup;
+export default React.memo(CreatePopup);
